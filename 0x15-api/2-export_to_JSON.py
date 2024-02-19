7ede973ce18342a9ee774ba0130e9aac5de24e13
Brianoyaro <brianoyaro2000@yahoo.com>
2024-02-19 10:54:49 +0300

    2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -7,9 +7,10 @@ import sys
 
 if __name__ == "__main__":
     value = sys.argv[1]
-    username_url = 'https://jsonplaceholder.typicode.com/users?id={}'.format(value)
-    data_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(value)
-    username = requests.get(username_url)
+    name_url = 'https://jsonplaceholder.typicode.com/users?id={}'.format(value)
+    data_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'\
+        .format(value)
+    username = requests.get(name_url)
     data = requests.get(data_url)
     username = username.json()
     data = data.json()
@@ -21,7 +22,8 @@ if __name__ == "__main__":
         if item.get('completed'):
             count += 1
             actual_done.append(item.get('title'))
-    print("Employee {} is done with tasks {}/{}".format(name, count, total_tasks))
+    print("Employee {} is done with tasks {}/{}"\
+        .format(name, count, total_tasks))
     for item in actual_done:
         print("\t{}".format(item))"""
 
