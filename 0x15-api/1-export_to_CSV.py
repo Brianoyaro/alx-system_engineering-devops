44034da388868cbca463d6098f09bc5e8bbac6ad
Brianoyaro <brianoyaro2000@yahoo.com>
2024-02-19 10:02:56 +0300

    1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -6,21 +6,22 @@ import sys
 
 if __name__ == "__main__":
     value = int(sys.argv[1])
-    username_url = 'https://jsonplaceholder.typicode.com/users?id={}'.format(value)
-    data_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(value)
-    username = requests.get(username_url)
+    name_url = 'https://jsonplaceholder.typicode.com/users?id={}'.format(value)
+    data_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'\
+        .format(value)
+    username = requests.get(name_url)
     data = requests.get(data_url)
     username = username.json()
     data = data.json()
     """name = username[0].get('name')
-    total_tasks = len(data)
+    total = len(data)
     count = 0
     actual_done = []
     for item in data:
         if item.get('completed'):
             count += 1
             actual_done.append(item.get('title'))
-    print("Employee {} is done with tasks {}/{}".format(name, count, total_tasks))
+    print("Employee {} is done with tasks {}/{}".format(name, count, total))
     for item in actual_done:
         print("\t{}".format(item))"""
 
@@ -28,7 +29,7 @@ if __name__ == "__main__":
     u_name = username[0].get('username')
     line = ""
     for item in data:
-        line += '"{}", "{}", "{}", "{}"\n'.\
+        line += '"{}","{}","{}","{}"\n'.\
                 format(value, u_name, item.get('completed'), item.get('title'))
     with open(filename, "w")as f:
         f.write(line)
