14cdc3c30279e7a2fc6dfe6898b41861df5681a9
Brianoyaro <brianoyaro2000@yahoo.com>
2024-03-05 12:03:43 +0300

    2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -9,13 +9,13 @@ def recurse(subreddit, hot_list=[], after=""):
         .format(subreddit, after)
     headers = {"user-agent": "user-agent"}
     resp = requests.get(url, headers=headers, allow_redirects=False)
-    val = resp.json().get('data', {}).get('children')
+    val = resp.json().get('data', {}).get('children', [])
     if val is None:
         if len(val) == 0:
             return None
         return hot_list
     for v in val:
-        hot_list.append(v.get('data', {}).get('title'))
+        hot_list.append(v.get('data', {}).get('title', ''))
     after = resp.json().get('data', {}).get('after')
     if after is None:
         return hot_list
