44f0042ddff8007865075bb14f6e7e26ae8cb488
Brianoyaro <brianoyaro2000@yahoo.com>
2024-03-06 15:02:52 +0300

    error in puppet file exec name

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Fixes typing error in wp-settings.php
-exec {'typing error:
-  path    => '/bin',
-  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php"
+exec {'typing error':
+  command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
